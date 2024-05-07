492c363fd07e456c5f6185868b419e50ff746c3c
alex.vaslin <vaslinalex@gmail.com>
2024-05-07 16:54:19 +0200

    finalisation P3 + debug du code ajouté + validateurs w3c

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -20,7 +20,7 @@ const modalGallery = document.querySelector(".modal-gallery");
 const addModal = document.querySelector(".modal-add-picture");
 
 //get the form for the 2nd modal
-const selectForm = Document.querySelector(".category");
+const selectForm = document.querySelector("#category");
 
 
 
@@ -59,10 +59,10 @@ const createProject = (project) => {
 
 //function to create category option
 const createOption = (category) => {
-    const optionform = document.createElement("option");
-    optionform.setAttribute("value", category.id);
-    optionform.innerText = category.name;
-    selectForm.appendChild(optionform);
+    const optionForm = document.createElement("option");
+    optionForm.setAttribute("value", category.id);
+    optionForm.innerText = category.name;
+    selectForm.appendChild(optionForm);
 };
 
 //function to remove element from parent to display a blank page
@@ -139,6 +139,7 @@ const getCategories = async(category) => {
     .then((category) => {
         category.forEach((category) => {
             createButton(category);
+            createOption(category);
         });
     })
 
@@ -210,177 +211,141 @@ const logOut = () => {
   window.location.href = "./index.html";
 };
 
-
-/* --- ADMIN PAGE ELEMENTS --- */
-
-//Function to open the modal 
-const openModal = () => {
-
-
-    //Activate the Aside 
-    asideModal.classList.remove("unactive-modal");
-    asideModal.setAttribute("aria-hidden", "false");
-
-    //Activate the modal
-    galleryModal.classList.remove("unactive-modal");
-    galleryModal.setAttribute("aria-hidden", false);
-
-    const addButton = document.querySelector("add-photo");
-    /*addButton.addEventListener("click", (event) => {
-        galleryModal.classList.add("unactive-modal");
-        /*ajout modal 2
-
-    }*/
-
-
-    //function to close the modal (cross)
-    //icon
-    const closeIcon = document.querySelector(".close-icon");
-    closeIcon.addEventListener("click", closeModal);
-
-    document.getElementById("modal").addEventListener("click", (event) => {
-        if(event.target === document.getElementById("modal")) {
-            closeModal();
-        }
-    });
-
-    //get works when the modal is open
-    getWorks();
-
-};
-
-//function closing modal by clicking outside
-const closeModal = () => {
-    asideModal.classList.add("unactive-modal");
-    modalGallery.classList.add("unactive-modal");
-    
-}
-
-
 // function to create admin page's elements 
 const adminPage = () => {
-  // Adding the admin "editor" bar
-  body.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="edit-bar">
-        <span class="edit"><i class="fa-solid fa-pen-to-square"></i>Mode édition</span>
-    </div>`
-  );
+    // Adding the admin "editor" bar
+    body.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="edit-bar">
+          <span class="edit"><i class="fa-solid fa-pen-to-square"></i>Mode édition</span>
+      </div>`
+    );
+  
+    // Adding the "open modal" button
+    galleryTitle.insertAdjacentHTML(
+      "afterend",
+      `<a id="open-modal" href="#modal" class="edit-link">
+          <i class="fa-solid fa-pen-to-square"></i>modifier
+      </a>`
+    );
+    // Removing filters like the figma design
+    document.querySelector(".filters-nav").style.display = "none";
+  
+  
+    /* --- LOGIN / LOGOUT --- */
+    // get the login/logout button 
+    const logButton = document.querySelector("#logButton");
+    console.log("#logButton");
+    // replace it by logout
+    logButton.innerHTML = `<a href="./index.html">logout</a>`;
+    // add event listener to wait "click"
+    logButton.addEventListener("click", logOut);
+  
+    /* --- OPPENING MODAL --- */
+    // Get the button to oppen the modal
+    const modalLink = document.querySelector("#open-modal");
+    // add event listener to wait the "click"
+    modalLink.addEventListener("click", openModal);
+       
+  };
+  
+  //function to delete projects from the modal's gallery
+  const deleteWork = async (workID) => {
+      await fetch("http://localhost:5678/api/works/" + workID, {
+          method: "DELETE",
+          headers: {
+              "content-type": "application/json",
+              Authorization: "bearer " + token,
+          },
+      }).catch((error) => {
+          console.log(error);
+      });
+  
+      //call getwork function to update projects
+      getWorks();
+  };
+  
+  //function to create project in the modal 
+  const createModalProject = (project) => {
+      const figureModalProject = document.createElement("figure");
+      figureModalProject.setAttribute("data-tag", project.id);
+  
+      const imageModalProject = document.createElement("img");
+      imageModalProject.src = project.imageUrl;
+      imageModalProject.alt = project.title;
+      imageModalProject.classList.add("modal-project-img");
+      
+      const trashIcon = document.createElement("i");
+      trashIcon.classList.add("trash-icon", "fas", "fa-trash-alt");
+      trashIcon.setAttribute("data-id", project.id);
+      let trashIconID = trashIcon.getAttribute("data-id");
+  
+      const moveIcon = document.createElement("div");
+      moveIcon.classList.add("move-icon");
+  
+      trashIcon.addEventListener("click", function (event) {
+          event.preventDefault();
+          if (confirm("Êtes-vous sûr de vouloir supprimer ce projet ?") == true) {
+            deleteWork(trashIconID);
+          }
+      });
+  
+      figureModalProject.appendChild(imageModalProject);
+      figureModalProject.appendChild(trashIcon);
+      trashIcon.appendChild(moveIcon);
+      modalGallery.appendChild(figureModalProject);
+  };
+  
+  
+  
+  
+  /******** ------- ADD PROJECT ------ **********/
+  const addWork = document.querySelector("#add-box");
+  const inputElement = document.querySelector("#title");
+  const selectElement = document.querySelector("#category");
+  const fileInputElement = document.querySelector("#image");
+  const submitButton = document.querySelector("#validate-button");
+  const inputFile = document.querySelector("#image");
+  
+  //function to display the preview of the new work/image
+  const showfile = (e) => {
+      e.preventDefault();
+  
+      //function display images
+      const reader = new FileReader();
+      //read image
+      reader.readAsDataURL(inputFile.files[0]);
+      //send back the src into the preview 
+      reader.addEventListener("load", function() {
+          previewImage.src = reader.result;
+      });
+      //display the preview when uploading
+      const previewBox = document.querySelector(".upload-picture-box");
+      const previewImage = document.querySelector("img");
+      previewImage.setAttribute("id", "preview-image");
 
-  // Adding the "open modal" button
-  galleryTitle.insertAdjacentHTML(
-    "afterend",
-    `<a id="open-modal" href="#modal" class="edit-link">
-        <i class="fa-solid fa-pen-to-square"></i>modifier
-    </a>`
-  );
-  // Removing filters like the figma design
-  document.querySelector(".filters-nav").style.display = "none";
-
-
-  /* --- LOGIN / LOGOUT --- */
-  // get the login/logout button 
-  const logButton = document.querySelector("#logButton");
-  console.log("#logButton");
-  // replace it by logout
-  logButton.innerHTML = `<a href="./index.html">logout</a>`;
-  // add event listener to wait "click"
-  logButton.addEventListener("click", logOut);
-
-  /* --- OPPENING MODAL --- */
-  // Get the button to oppen the modal
-  const modalLink = document.querySelector("#open-modal");
-  // add event listener to wait the "click"
-  modalLink.addEventListener("click", openModal);
-     
-};
-
-//function to delete projects from the modal's gallery
-const deleteWork = async (workID) => {
-    await fetch("http://localhost:5678/api/works" + workID, {
-        method: "DELETE",
-        headers: {
-            "content-type": "application/json",
-            Authorization: "bearer" + token,
-        },
-    }).catch((error) => {
-        console.log(error);
-    });
-
-    //call getwork function to update projects
-    getWorks();
-};
-
-//function to create project in the modal 
-const createModalProject = (project) => {
-    const figureModalProject = document.createElement("figure");
-    figureModalProject.setAttribute("data-tag", project.id);
-
-    const imageModalProject = document.createElement("img");
-    imageModalProject.src = project.imageUrl;
-    imageModalProject.alt = project.title;
-    imageModalProject.classList.add("modal-project-img");
-    
-    const trashIcon = document.createElement("i");
-    trashIcon.classList.add("trash-icon", "fas", "fa-trash-alt");
-    trashIcon.setAttribute("data-id", project.id);
-    let trashIconID = trashIcon.getAttribute("data-id");
-
-    const moveIcon = document.createElement("div");
-    moveIcon.classList.add("move-icon");
-
-    trashIcon.addEventListener("click", function (event) {
-        event.preventDefault();
-        if (confirm("Êtes-vous sûr de vouloir supprimer ce projet ?") == true) {
-          deleteWork(trashIconID);
+      // Apply styles directly to the first image
+        if (!document.querySelector(".preview-image")) {
+            previewImage.style.position = "absolute";
+            previewImage.style.objectFit = "contain";
+            previewImage.style.width = "100%";
+            previewImage.style.height = "100%";
         }
-    });
+  
+      //hide background elements
+      const uploadbutton = document.querySelector(".upload-button");
+      uploadbutton.style.display ="none";
+      const pictureIcon = document.querySelector(".picture-icon");
+      pictureIcon.style.display ="none";
+      const typeFiles = document.querySelector(".type-files");
+      typeFiles.style.display ="none";
+  
+      previewBox.appendChild(previewImage);
+  
+  };
 
-    figureModalProject.appendChild(imageModalProject);
-    figureModalProject.appendChild(trashIcon);
-    trashIcon.appendChild(moveIcon);
-    modalGallery.appendChild(figureModalProject);
-};
-
-/******** ------- ADD PROJECT ------ **********/
-const addWork = document.querySelector("#add-box");
-const inputElement = document.querySelector("#title");
-const selectElement = document.querySelector("#category");
-const fileInputElement = document.querySelector("#image");
-const submitButton = document.querySelector("#validate-button");
-const inputFile = document.querySelector("#image");
-
-//function to display the preview of the new work/image
-const showfile = (e) => {
-    e.preventDefault();
-
-    //function display images
-    const reader = new FileReader();
-    //read image
-    reader.readAsDataURL(inputFile.files[0]);
-    //send back the src into the preview 
-    reader.addEventListener("load", function() {
-        previewImage.src = reader.result;
-    });
-    //display the preview when uploading
-    const previewBox = document.querySelector(".upload-box");
-    const previewImage = document.querySelector("img");
-    previewImage.setAttribute("id", "preview-image");
-
-    //hide background elements
-    const uploadbutton = document.querySelector(".upload-button");
-    uploadbutton.style.display ="none";
-    const pictureIcon = document.querySelector(".picture-icon");
-    pictureIcon.style.display ="none";
-    const typeFiles = document.querySelector(".type-files");
-    typeFiles.style.display ="none";
-
-    previewBox.appendChild(previewImage);
-
-};
-
-//Fnction to check form fields 
-const checkform = () => {
+  //Fnction to check form fields 
+const checkForm = () => {
 
 
     if (inputElement.value !== "" && selectElement.value !== "" && fileInputElement.value !== "") {
@@ -394,19 +359,19 @@ const checkform = () => {
 
 //Listenners of the form
 inputFile.addEventListener("change", showfile);
-inputElement.addEventListener("input", checkform);
-selectElement.addEventListener("input", checkform);
-fileInputElement.addEventListener("change", checkform);
+inputElement.addEventListener("input", checkForm);
+selectElement.addEventListener("input", checkForm);
+fileInputElement.addEventListener("change", checkForm);
 
 //Add a new project
 const addWorks = async () => {
     //get form elements 
-    let getpic = document.getElementById("image").file[0];
+    let getpic = document.getElementById("image").files[0];
     let gettitle = document.getElementById("title").value;
     let getCategory = document.getElementById("category").value;
 
     //building the form data to send to the API
-    let formData = new Formfata();
+    let formData = new FormData();
     formData.append("image", getpic);
     formData.append("title", gettitle);
     formData.append("category", getCategory);
@@ -415,7 +380,7 @@ const addWorks = async () => {
     await fetch("http://localhost:5678/api/works", {
         method: "POST",
         headers: {
-            Authorization: "Bearer" + token,
+            Authorization: "Bearer " + token,
             Accept: "application/json",
         },
         body: formData,
@@ -424,7 +389,7 @@ const addWorks = async () => {
         if(response.ok) {
             getWorks(); // Updating galleries (portfolio and modal)
             closeModal(); // closing the modal 
-            console.log("Le projet à été ajouté !");
+            console.log("Le projet a été ajouté !");
             return response.json();
         } else {
             console.log("Api data recovery error");
@@ -433,35 +398,126 @@ const addWorks = async () => {
     .catch((error) => {
         console.log(error);
     });
+    
 };
 
-//function to check if form element have value
-const imgIssue = document.querySelector("#error-img");
-const titleIssue = document.querySelector("#error-title");
-const categoryIssue = document.querySelector("#error-category");
 
-//if fields are full, thread addWork function
-if(inputElement.value !== "" && selectElement !== "" && fileInputElement !== "") {
-    addWorks();
-}
+const validateForm = (e) => {
+    e.preventDefault();
 
-//if a field is empty, displaying one of these error messages 
-if(inputFile.value == "") {
-    imgIssue.innerHTML = "image obligatoire";
-} else {
-    titleIssue.innerHTML = "";
-}
-if(inputElement.value == ""){
-    titleIssue.innerHTML = "Titre obligatoire";
-} else {
-    titleIssue = "";
-}
-if(selectElement.value ==""){
-    categoryIssue.innerHTML = "catégorie obligatoire";
-} else {
-    categoryIssue.innerHTML ="";
-}
+    //function to check if form element have value
+    let imgIssue = document.querySelector("#error-img");
+    let titleIssue = document.querySelector("#error-title");
+    let categoryIssue = document.querySelector("#error-category");
 
+    //if fields are full, thread addWork function
+    if(inputElement.value !== "" && selectElement !== "" && fileInputElement !== "") {
+        addWorks();
+    }
+
+    //if a field is empty, displaying one of these error messages 
+    if(inputFile.value == "") {
+        imgIssue.innerHTML = "image obligatoire";
+    } else {
+        titleIssue.innerHTML = "";
+    }
+    if(inputElement.value == ""){
+        titleIssue.innerHTML = "Titre obligatoire";
+    } else {
+        titleIssue = "";
+    }
+    if(selectElement.value ==""){
+        categoryIssue.innerHTML = "catégorie obligatoire";
+    } else {
+        categoryIssue.innerHTML ="";
+    }
+};
+
+/* --- ADMIN PAGE ELEMENTS --- */
+
+//Function to open the modal 
+const openModal = () => {
+
+    //Activate the Aside 
+    asideModal.classList.remove("unactive-modal");
+    asideModal.setAttribute("aria-hidden", "false");
+
+    //Activate the modal
+    galleryModal.classList.remove("unactive-modal");
+
+
+    const addPicButton = document.querySelector("#add-photo");
+    addPicButton.addEventListener("click", (event) => {
+        //hide 1st modal
+        galleryModal.classList.add("unactive-modal");
+        //display modal 2
+        addModal.classList.remove("unactive-modal");
+        //closing modal 2 on the X
+        const closeIcon2 = document.querySelector(".close-icon-2");
+        closeIcon2.addEventListener("click", closeModal);
+        //back button
+        const backIcon = document.querySelector(".back-icon");
+        backIcon.addEventListener("click", (event) => {
+            // display modal again
+            galleryModal.classList.remove("unactive-modal");
+            //add the unactive modal on the 2nd modal
+            addModal.classList.add("unactive-modal");
+            
+        });       
+    });
+    
+    
+        //listener for the validate button
+        //check the form (validateForm)
+        addWork.addEventListener("submit", validateForm);
+
+        //function to close the modal (cross)
+        //icon
+        const closeIcon = document.querySelector(".close-icon");
+        closeIcon.addEventListener("click", closeModal);
+
+        document.getElementById("modal").addEventListener("click", (event) => {
+            if(event.target === document.getElementById("modal")) {
+                closeModal();
+            }
+        });
+
+        //get works when the modal is open
+        getWorks();
+    
+
+};
+
+
+
+
+//function closing modal by clicking outside
+const closeModal = () => {
+    asideModal.classList.add("unactive-modal");
+    modalGallery.classList.add("unactive-modal");
+    addModal.classList.add("unactive-modal");
+
+    //reset form
+    document.querySelector("#add-box").reset();
+    //removing preview image 
+    const previewBox = document.querySelector(".upload-picture-box");
+    const previewImage = document.querySelector("#preview-image");
+    if (previewImage !== null) {
+        previewBox.removeChild(previewImage);
+    }
+
+    //displaying pictureBox elements again
+    const uploadButtonPic = document.querySelector(".upload-button");
+    uploadButtonPic.style.display = "";
+    const pictureIcon = document.querySelector(".picture-icon");
+    pictureIcon.style.display = "";
+    const typeFiles = document.querySelector(".type-files");
+    typeFiles.style.display = "";
+
+    //reset validate button
+    submitButton.style.backgroundColor = "#a7a7a7";
+    
+};
 
 
 //A function to thread admin page 
@@ -469,6 +525,7 @@ if(selectElement.value ==""){
 if(token !== null) {
     adminPage();
 }
+
 
 
 
